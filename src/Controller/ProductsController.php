76e660a9e7a625b5ee16675223eba39b5665c7bd
Jules Mahe <julesmahe.mahe@gmail.com>
2024-01-11 14:23:35 +0100

    exercice jour 3

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Produits;
+use App\Form\ProductFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController extends AbstractController
 {
     #[Route('/products', name: 'app_products')]
-    public function index(): Response
+    public function new(Request $request, ManagerRegistry $managerRegistry): Response
     {
+
+        $product = new Produits();
+        $productForm = $this->createForm(ProductFormType::class, $product);
+        $productForm->handleRequest($request);
+
+        if ($productForm->isSubmitted() && $productForm->isValid()){
+            $entityManager = $managerRegistry->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+        }
+
         return $this->render('products/index.html.twig', [
-            'controller_name' => 'ProductsController',
+            'form'=>$productForm->createView()
         ]);
     }
+
+
 }
---
+++ src/Form/ProductFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produits;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('quantity')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produits::class,
+        ]);
+    }
+
+}
